81d72f546cc55e8b4662580694996fdd27ed9255
nathaฯ <nathachumsri@gmail.com>
2024-03-08 20:04:06 +0700

    add Authentication

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const{getCampgrounds, getCampground, createCampground, updateCampground, deleteCampground} = require('../controllers/campgrounds');
 const router = express.Router();
+const {protect, authorize} = require('../middleware/auth');
 
-router.route('/').get(getCampgrounds).post(createCampground);
-router.route('/:id').get(getCampground).put(updateCampground).delete(deleteCampground);
+router.route('/').get(getCampgrounds).post(protect,authorize('admin'),createCampground);
+router.route('/:id').get(getCampground).put(protect,authorize('admin'),updateCampground).delete(protect,authorize('admin'),deleteCampground);
 
 module.exports = router;
